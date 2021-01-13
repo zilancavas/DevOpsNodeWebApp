8cb6b8d10f93aebbb6ffee7e1f18e020899c434c
Adrian Moser <mosa@zhaw.ch>
2021-01-13 10:49:18 +0100

    test

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello DevOps Course :-)))');
+  res.send('(((-: Hello DevOps Course :-)))');
 });
 
 app.listen(PORT, HOST);
