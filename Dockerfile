cb89815eba77be1e84b803a3a462311b3867879f
Adrian Moser <mosa@zhaw.ch>
2023-01-09 10:45:48 +0100

    update

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:14
+FROM node:18.12.1
 
 # Create app directory
 WORKDIR /usr/src/app
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-	res.send('Hello Monday DevOps Course! :-))))))))))))))))');
+	res.send('Hello FS2023 DevOps Course!');
 });
 
 app.listen(PORT, HOST);
