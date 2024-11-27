aebb94f8c37bc0c4c5751c824d1297cc46531845
mosazhaw <mosa@zhaw.ch>
2024-11-27 11:08:21 +0100

    fs25

--- server.js
+++ server.js
@@ -9,18 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-	res.send(`
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>Hello World</title>
-  </head>
-  <body>
-		<b>Hello FS2025 DevOps Course! :-)</b>
-  </body>
-</html>
-`);
+	res.send('Hello FS2025 DevOps Course! :-)');
 });
 
 app.listen(PORT, HOST);
