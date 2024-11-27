15c04666f9171c4204255542c6d918919d81fdaf
mosazhaw <mosa@zhaw.ch>
2024-11-27 13:11:27 +0100

    example push

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello FS2025 DevOps Course! :-)');
+  res.send('Hello FS2025 DevOps Course! Hello Render! :-)');
 });
 
 app.listen(PORT, HOST);
