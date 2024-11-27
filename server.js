c4dc99838fb201f8923ec428ae44e21c04b3bbf4
mosazhaw <mosa@zhaw.ch>
2024-11-27 10:33:40 +0100

    html hello world

--- server.js
+++ server.js
@@ -9,7 +9,18 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-	res.send('Hello FS2025 DevOps Course! :-)))');
+	res.send(`
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Hello World</title>
+  </head>
+  <body>
+		<b>Hello FS2025 DevOps Course! :-)</b>
+  </body>
+</html>
+`);
 });
 
 app.listen(PORT, HOST);
