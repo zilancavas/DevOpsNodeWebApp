096198a2ea9e53965e2b4f1f3fb41b45b4e3d877
mosazhaw <mosa@zhaw.ch>
2025-05-07 09:39:23 +0200

    test commit

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello FS2025 DevOps Course! Cloud Deploy auf Render.com');
+  res.send('Hello FS2025 DevOps Course! Cloud Deploy auf Azure');
 });
 
 app.listen(PORT, HOST);
