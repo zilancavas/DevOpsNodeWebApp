0dfa38b0ef31cfa3622d7d2a5a2655facb314900
mosazhaw <mosa@zhaw.ch>
2025-05-13 15:01:52 +0200

    test commit

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello FS2025 DevOps Course!');
+  res.send('Hello FS2025 DevOps Course! Now Docker Deployment with GitHub Actions!!!');
 });
 
 app.listen(PORT, HOST);
