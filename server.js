f90efbcd21a852897d6fecd83d2bec164ea1967f
zilancavas <cavaszil@students.zhaw.ch>
2025-05-17 23:31:10 +0200

    Teständerung von Zilan – läuft alles automatisch?

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello FS2025 DevOps Course! Now Docker Deployment with GitHub Actions!!!');
+  res.send('Teständerung von Zilan – läuft alles automatisch?');
 });
 
 app.listen(PORT, HOST);
